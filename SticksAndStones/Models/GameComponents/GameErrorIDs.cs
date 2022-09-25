914898af9cbd4b1b308c73152a86782d648b36d7
jp8117 <jp8117@live.com>
2022-09-25 18:59:29 -0400

    Simple enum housing various error codes that can pass information back up to the upper levels of the system or as feedback for the player

---
+++ SticksAndStones/Models/GameComponents/GameErrorIDs.cs
@@ -0,0 +1,64 @@
+﻿namespace SticksAndStones.Models.GameComponents
+{
+    public enum GameError
+    {
+        /// <summary>
+        /// No errors occured in method
+        /// </summary>
+        SUCCESS,
+
+        /// <summary>
+        /// A general failure occured, this flag should almost never be 
+        /// used as it's ambiguity makes debugging more difficult
+        /// </summary>
+        GENERAL_FAILURE,
+
+        /// <summary>
+        /// used in the move classes to denote a duplicate target when
+        /// attempting to add a new target to a move. Can be used by 
+        /// front end to inform user of invalid selection.
+        /// </summary>
+        MOVE_TARGET_DUPLICATE,
+
+        /// <summary>
+        /// used in the move classes when attempting to locate a targeted
+        /// player who was not found. Can be used by front end to inform
+        /// user of invalid selection.
+        /// </summary>
+        MOVE_TARGET_NOT_FOUND,
+
+        /// <summary>
+        /// used by move classes when attempting to add a target to the targets list
+        /// if the list is already full. Can be fed back to user to inform them
+        /// of invalid selection
+        /// </summary>
+        MOVE_MAX_TARGETS_EXCEEDED,
+
+        /// <summary>
+        /// used by move classes to inform player that they don't have enough power to complete the
+        /// requested move.
+        /// </summary>
+        MOVE_NOT_ENOUGH_POWER,
+
+        /// <summary>
+        /// used by move classes to indicate an opperation occured that shouldn't have proceeded without
+        /// at least one target defined.
+        /// </summary>
+        MOVE_NO_TARGETS_DEFINED,
+
+        /// <summary>
+        /// used by IEffectable objects for changes that are too high above acceptable range
+        /// </summary>
+        IEFFECTABLE_ARGUMENT_TOO_HIGH,
+
+        /// <summary>
+        /// used by IEffectable objects for changes that are too low below acceptable range
+        /// </summary>
+        IEFFECTABLE_ARGUMENT_TOO_LOW,
+
+        /// <summary>
+        /// Used by IEffectable to indicate an effect that was being searched for was not found
+        /// </summary>
+        IEFFECTABLE_EFFECT_NOT_FOUND
+    }
+}
