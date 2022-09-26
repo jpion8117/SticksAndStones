d0862dbfb49357e215e2d4890985af4cec1b8703
jp8117 <jp8117@live.com>
2022-09-26 18:34:11 -0400

    Very simple class that generates unique ulong identifiers

---
+++ SticksAndStones/Models/GameComponents/UniqueIDGenerator.cs
@@ -0,0 +1,21 @@
+﻿namespace SticksAndStones.Models.GameComponents
+{
+    public class UniqueIDGenerator
+    {
+        static private ulong _idPool = 0;
+        
+        /// <summary>
+        /// Gets the next ID queued up in the ID Pool and queues up the next one. 
+        /// in otherwords, this ID will be unique each time it is read from.
+        /// </summary>
+        static public ulong NextID
+        {
+            get
+            {
+                ulong uID = _idPool;
+                _idPool++;
+                return uID;
+            }
+        }
+    }
+}
