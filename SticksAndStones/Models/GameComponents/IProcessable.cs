5cd55f6d6a98d4becaae682ba2984ca71165cec7
jp8117 <jp8117@live.com>
2022-09-26 19:49:40 -0400

    added a priority system for prioritizing the order of procesables

--- SticksAndStones/Models/GameComponents/IProcessable.cs
+++ SticksAndStones/Models/GameComponents/IProcessable.cs
@@ -2,6 +2,21 @@
 {
     public interface IProcessable
     {
-        public GameError ProcessLoop();
+        /// <summary>
+        /// Dictates what priority this entity will process at the lower the priority
+        /// the sooner it will process
+        /// </summary>
+        public int Priority { get; }
+
+        /// <summary>
+        /// gets the entity's processable ID
+        /// </summary>
+        public ulong UniqueID { get; }
+
+        /// <summary>
+        /// gets the completion status of a processable and 
+        /// </summary>
+        public bool Completed { get; }
+        public GameError ExecuteAction();
     }
 }
