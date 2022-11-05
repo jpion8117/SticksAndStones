0d09412142873b8c65e2b6477ca9b94fbd80894e
jp8117 <jp8117@live.com>
2022-11-05 14:36:50 -0400

    Added RealName property and data annotations

--- SticksAndStones/Models/DAL/User.cs
+++ SticksAndStones/Models/DAL/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.RegularExpressions;
 
 namespace SticksAndStones.Models.DAL
@@ -12,10 +13,28 @@ namespace SticksAndStones.Models.DAL
         [StringLength(15, ErrorMessage = "Username must not exceed 15 characters.")]
         [RegularExpression("([a-z]|[A-Z]|[0-3]|_|-|)", ErrorMessage = "User name may only contain letters, number, or " +
             "the '_' and '-' characters")]
+        [DataType(DataType.Text)]
         public string Username { get; set; }
+
+        [Display(Name = "Real Name")]
+        [Column("Name")]
+        [StringLength(50, ErrorMessage = "Real name may not exceede 50 characters.")]
+        [RegularExpression("(([a-z]|[A-Z]){3,25} {1}([a-z]|[A-Z]){3,25}", ErrorMessage = "Please provide first and last name " +
+            "separated by a space.")]
+        public string RealName { get; set; }
+
+        [Display(Name = "Games Played")]
+        [Range(0,9999999)]
         public int GamesPlayed { get; set; }
+
+        [Display(Name = "Games Won")]
+        [Range(0,9999999)]
         public int GamesWon { get; set; }
+
+        [Display(Name = "Games Lost")]
         public int GamesLost { get { return GamesPlayed - GamesWon; } }
+
+        [Display(Name = "Win Rate")]
         public double WinRate 
         { 
             get 
