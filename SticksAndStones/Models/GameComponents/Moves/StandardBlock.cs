d1771852abba76a6e1fc734297afd5ebe80b9df7
jp8117 <jp8117@live.com>
2022-11-14 18:09:54 -0500

    implimented round level action to reset defense multiplier to original value

--- SticksAndStones/Models/GameComponents/Moves/StandardBlock.cs
+++ SticksAndStones/Models/GameComponents/Moves/StandardBlock.cs
@@ -5,28 +5,31 @@ namespace SticksAndStones.Models.GameComponents.Moves
     public class StandardBlock : BaseMove
     {
         private double _originalDefenseMultiplier;
-        private int _runCount = 0;
         public StandardBlock(CharacterBase executioner) : base(executioner)
         {
             _moveCost = 0;
             _maxTargets = 1;
+            _processModes[ProcessMode.Round] = true; //add process mode for round level event
         }
 
         public override string Type => "StandardBlockMove";
 
         public override GameError ExecuteAction(ProcessMode mode = ProcessMode.Move)
         {
-            if (_runCount == 0)
+            switch (mode)
             {
-                _originalDefenseMultiplier = _moveExecutioner.DefenseMultiplier;
-                _moveExecutioner.DefenseMultiplier *= 1.25f;
-                base.ExecuteAction();
-                _moveExecuted = false;
-                return GameError.SUCCESS;
+                case ProcessMode.Move:
+                    _originalDefenseMultiplier = _moveExecutioner.DefenseMultiplier;
+                    _moveExecutioner.DefenseMultiplier *= 1.25f;
+                    base.ExecuteAction();
+                    _moveExecuted = false;
+                    return GameError.SUCCESS;
+                case ProcessMode.Round:
+                    _moveExecutioner.DefenseMultiplier = _originalDefenseMultiplier;
+                    return base.ExecuteAction();
             }
 
-            _moveExecutioner.DefenseMultiplier = _originalDefenseMultiplier;
-            return base.ExecuteAction();
+            return GameError.SUCCESS;
         }
     }
 }
