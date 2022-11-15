bcd6178a3df1313baa765e665a5cd2b11956528a
jp8117 <jp8117@live.com>
2022-11-15 15:27:30 -0500

    Added test for StandardBlock move and updated BulletShield move

--- TankUnitTest/TankMoveTests.cs
+++ TankUnitTest/TankMoveTests.cs
@@ -3,6 +3,7 @@ using SticksAndStones.Models.GameComponents.Characters;
 using SticksAndStones.Models.GameComponents.Moves.Tank;
 using SticksAndStones.Models.GameComponents.Moves;
 using SticksAndStones.Models.GameComponents;
+using System;
 
 namespace TankUnitTest
 {
@@ -46,11 +47,37 @@ namespace TankUnitTest
             Assert.IsTrue(target.Health == expectedResult, $"Expected health to be {expectedResult} but " +
                 $"reported as {target.Health}");
         }
+        [TestMethod]
+        public void Test003_StandardBlock()
+        {
+            var executioner = new Tank();
+            var move = new StandardBlock(executioner);
+
+            move.ExecuteAction(ProcessMode.Move);
+            move.ExecuteAction(ProcessMode.Turn);
+            Assert.IsFalse(move.Completed, "Move was prematurely marked as complete");
+            double expectedMultiplier = Math.Round(1 - 0.8f, 2); 
+            Assert.IsTrue(executioner.DefenseMultiplier == expectedMultiplier, $"Defense Multiplier expected: {expectedMultiplier}," +
+                $" result: {executioner.DefenseMultiplier}");
+
+            //cause it pain...
+            executioner.TakeDamage(10);
+
+            int expectedHealth = 98;
+            Assert.IsTrue(executioner.Health == expectedHealth, $"Health Expected: {expectedHealth}," +
+                $" result: {executioner.Health}");
+
+            expectedMultiplier = 0.5f;
+            move.ExecuteAction(ProcessMode.Round);
+            Assert.IsTrue(executioner.DefenseMultiplier == expectedMultiplier, $"Defense Multiplier expected: {expectedMultiplier}," +
+                $" result: {executioner.DefenseMultiplier}");
+            Assert.IsTrue(move.Completed, "move failed to be marked completed");
+        }
         /// <summary>
         /// Tests the bullet sponge move and by extension the attack redirect system
         /// </summary>
         [TestMethod]
-        public void Test003_BulltetSpongeMove()
+        public void Test004_BulltetSpongeMove()
         {
             //create multiple instances of Tank class
             var executioner = new Tank();
@@ -84,6 +111,11 @@ namespace TankUnitTest
             teamMember2.TakeDamage(10, true);
             executioner.TakeDamage(10, true);
 
+            teamMember1.ExecuteAction();
+            teamMember1.ExecuteAction(ProcessMode.Turn);
+            teamMember2.ExecuteAction();
+            teamMember2.ExecuteAction(ProcessMode.Turn);
+
             //verify all damage was redirected to executioner
             int expectedResultE = 70; //expected health of the executioner
             int expectedResultT = 100; //expected health of the team members
@@ -95,7 +127,7 @@ namespace TankUnitTest
                 $"{expectedResultE}HP but was reported as {executioner.Health}HP");
 
             //run ExecuteAction on teamMember1 to remove redirect then apply 10hp direct damage
-            teamMember1.ExecuteAction();
+            teamMember1.ExecuteAction(ProcessMode.Round);
             teamMember1.TakeDamage(10, true);
 
             //verify damage was approprately applied to only teamMember1 and not executioner
@@ -109,7 +141,7 @@ namespace TankUnitTest
         /// Tests the blood donor move inwich one tank character gives another character 10hp 
         /// </summary>
         [TestMethod]
-        public void Test004_BloodDonor()
+        public void Test005_BloodDonor()
         {
             //create test characters and configure
             var speciminA = new Tank();
@@ -149,7 +181,7 @@ namespace TankUnitTest
         /// Tests the tank's body slam move against another target
         /// </summary>
         [TestMethod]
-        public void Test005_BodySlam()
+        public void Test006_BodySlam()
         {
             var speciminA = new Tank();
             var speciminB = new Tank();
@@ -177,7 +209,7 @@ namespace TankUnitTest
                 $"result:{speciminB.Power}");
         }
         //[TestMethod]
-        //public void Test006_ManOfSteelSpecial()
+        //public void Test007_ManOfSteelSpecial()
         //{ 
 
         //}
